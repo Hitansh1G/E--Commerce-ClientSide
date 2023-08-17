4fd03638d54ca5851a4043c09ab51bfef5e275e1
Hitansh1G <hitanshgupta007@gmail.com>
2023-08-17 12:43:11 +0530

    first commit

---
+++ src/customer/pages/HomePage/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import MainCrosel from '../../components/HomeCarousel/MainCrosel'
+
+const HomePage = () => {
+    return (
+        <div>
+            <MainCrosel/>
+            <div>
+                other section
+            </div>
+        </div>
+    )
+}
+
+export default HomePage
